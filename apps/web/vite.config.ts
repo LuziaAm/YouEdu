9d6849ac024bebe1b9ac7c8894b010548c91d5c4
LuziaAm <luzia.castro29@gmail.com>
2026-02-02 16:19:17 -0400

    fix(web): configure envDir to load env vars from root

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: '../../',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
